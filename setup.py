378bfce926d425fa4b8c94ebb1362043b404bbba
MartinHjelmare <marhje52@kth.se>
2015-05-10 13:15:01 +0200

    Updated setup with more meta data and functionality

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
+
+    def readme():
+        with open('README.md') as f:
+            return f.read()
 except ImportError:
     from distutils.core import setup
 
 config = {
     'description': 'My Project',
+    'long_description': readme(),
     'author': 'Martin Hjelmare',
     'url': 'URL to get it at.',
     'download_url': 'Where to download it.',
+    'license': 'GPLv3',
     'author_email': 'marhje52@kth.se',
     'version': '0.1',
     'install_requires': ['nose'],
-    'packages': ['NAME'],
-    'scripts': [],
-    'name': 'projectname'
+    'packages': find_packages(),
+    'include_package_data': True,
+    'scripts': ['scripts/path/to/script'],
+    'name': 'projectname',
+    'zip_safe': False
 }
 
 setup(**config)
